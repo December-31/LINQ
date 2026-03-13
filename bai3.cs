c7ab4fe84af990f7b489652e34f9984f3533edd8
Trương Văn Cường <vancuong31122k6@gmail.com>
2026-03-13 22:09:25 +0700

    add file bai3

---
+++ bai3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai3
+{
+    internal class bai3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten: Truong Van Cuong   MaSV: 2415053122304");
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+            var result = numbers.OrderBy(n => n);
+            Console.WriteLine("Sap xep tang dan: " + string.Join(", ", result));
+            Console.ReadLine();
+        }
+    }
+}
