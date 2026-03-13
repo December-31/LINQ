8f81f9f1178887ea79160153b28d82bd30688587
Trương Văn Cường <vancuong31122k6@gmail.com>
2026-03-13 22:14:07 +0700

    add file Student, bai11

---
+++ bai11.cs
@@ -0,0 +1,29 @@
+﻿using bai8;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai11
+{
+    internal class bai11
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten: Truong Van Cuong   MaSV: 2415053122304");
+
+            List<Student> students = new List<Student>() {
+                new Student{Id=1, Name="An", Score=8}, 
+                new Student{Id=2, Name="Binh", Score=6},
+                new Student{Id=3, Name="Chi", Score=9}, 
+                new Student{Id=4, Name="Dung", Score=7}
+            };
+
+            var result = students.OrderByDescending(s => s.Score).First();
+
+            Console.WriteLine("sinh vien co diem cao nhat: " + result.Name);
+            Console.ReadLine();
+        }
+    }
+}
