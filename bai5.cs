d4c5b308df14d7498449710ffbcf9d40315a403a
Trương Văn Cường <vancuong31122k6@gmail.com>
2026-03-13 22:10:24 +0700

    add file bai5

---
+++ bai5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai5
+{
+    internal class bai5
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten: Truong Van Cuong   MaSV: 2415053122304");
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            var result = numbers.Select(n => n * n);
+
+            Console.WriteLine("binh phuong cac so: " + string.Join(", ", result));
+            Console.ReadLine();
+        }
+    }
+}
