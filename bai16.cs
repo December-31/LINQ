60e9389e487fbed5ebafeebe64b02b9b6fe42c57
Trương Văn Cường <vancuong31122k6@gmail.com>
2026-03-13 22:18:04 +0700

    add file bai16

---
+++ bai16.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai16
+{
+    internal class bai16
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten: Truong Van Cuong   MaSV: 2415053122304");
+            List<int> numbers = new List<int> { 1, 2, 2, 3, 4, 4, 5 };
+
+            var result = numbers.Sum();
+
+            Console.WriteLine("tong cac so trong danh sach: " + result);
+            Console.ReadLine();
+
+        }
+    }
+}
