9ce3bc05731fa0faadabef35ba77940cf28e43bf
Trương Văn Cường <vancuong31122k6@gmail.com>
2026-03-13 22:16:01 +0700

    add file Student, program

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using bai8;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten: Truong Van Cuong   MaSV: 2415053122304");
+
+            List<Student> students = new List<Student>() {
+                new Student{Id=1, Name="An", Score=8},
+                new Student{Id=2, Name="Binh", Score=6},
+                new Student{Id=3, Name="Chi", Score=9},
+                new Student{Id=4, Name="Dung", Score=7}
+            };
+
+            var result = students.Any(s => s.Score < 5);
+            if(result)
+                Console.WriteLine("co sinh vien rot ");
+            else
+                Console.WriteLine("khong co sinh vien rot ");
+            Console.ReadLine();
+        }
+    }
+}
